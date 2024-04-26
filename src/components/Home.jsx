4858338f8f94ef4669d48b86ff133877e0147041
[cj-2123] <cj2123acct@gmail.com>
2024-04-25 21:31:19 -0700

    popout tracker

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,15 @@ import World from "./World.jsx";
 import Tracker from "./Tracker.jsx";
 
 function Home() {
+  // Open popout window for hints
+  function handleOpenPopout() {
+    window.open(
+      "#/tracker",
+      "_blank",
+      "toolbar=no,resizeable=yes,width=200,height=450"
+    );
+  }
+
   // Create checklist on left and tracker on right
   return (
     <div>
@@ -10,6 +19,11 @@ function Home() {
       </div>
       <div className="tracker">
         <Tracker></Tracker>
+        <div>
+          <button className="popout-button" onClick={handleOpenPopout}>
+            Popout Tracker
+          </button>
+        </div>
       </div>
     </div>
   );
